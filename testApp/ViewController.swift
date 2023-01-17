4bae366021f0095cb9a0bebf1e9988e6c68e38d3
Freimanis <nik.freymanis@gmail.com>
2023-01-17 14:00:59 +0700

    Initial Commit

---
+++ testApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testApp
+//
+//  Created by Nikita Freimanis on 17/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
